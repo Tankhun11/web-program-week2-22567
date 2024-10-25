a22c7a51541037aacccb3d63107af0547e770c17
Thaenkhun <tankoon300950@gmail.com>
2024-10-25 16:18:51 +0700

    531,441,000,000,000,000

---
+++ app203.php
@@ -0,0 +1,12 @@
+<?php
+$x = 20;
+$y = 7;
+$add = $x + $y;
+$sub = $x - $y;
+$mul = $x * $y;
+$div = $x / $y;
+$mod = $x % $y;
+echo "Result of Addition is $add <br />";
+echo "Result of Subtarction is $sub <br />";
+echo "Result of Multiplication is $mul <br />";
+echo "Result of Division is $div <br />";
